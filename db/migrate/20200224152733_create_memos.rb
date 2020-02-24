8335d0627f1afeb6ef306a2d56099ac607893414
yoshibuchi <yoshibuchi.n@gmail.com>
2020-02-25 00:58:53 +0900

    Update 20200224152733_create_memos.rb

--- db/migrate/20200224152733_create_memos.rb
+++ db/migrate/20200224152733_create_memos.rb
@@ -4,7 +4,7 @@ class CreateMemos < ActiveRecord::Migration[5.2]
       t.text :point1,                             null: false
       t.text :point2
       t.text :point3
-      t.text :impression
+      t.text :impression                          null: false
       t.references :user, foreign_key: true 
       t.references :memo, foreign_key: true 
 
