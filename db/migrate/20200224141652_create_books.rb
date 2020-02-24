c7399777216b82ba9d3d407326cbd139d9d4e7b8
yoshibuchi <yoshibuchi.n@gmail.com>
2020-02-24 23:22:33 +0900

    Update 20200224141652_create_books.rb

--- db/migrate/20200224141652_create_books.rb
+++ db/migrate/20200224141652_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
+      t.string :title,     null: false
+      t.string :author,    null: false
 
       t.timestamps
     end
