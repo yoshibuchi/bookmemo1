4b03c3a6cfe1b3c6358a2766f7f423d1775bc1ec
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-03 00:44:20 +0900

    Update 20200302152118_add_details_to_books.rb

--- db/migrate/20200302152118_add_details_to_books.rb
+++ db/migrate/20200302152118_add_details_to_books.rb
@@ -4,6 +4,7 @@ class AddDetailsToBooks < ActiveRecord::Migration[5.2]
     add_column :books, :point2, :text
     add_column :books, :point3, :text
     add_column :books, :impression, :text, null: false
+    add_column :books, :user_id, :integer, null: false
     add_reference :users, foreign_key: true
   end
 end
