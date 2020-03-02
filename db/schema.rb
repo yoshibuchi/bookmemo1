f81b48727bc6332c9a545dc7464f49217d1395b0
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-02 20:54:58 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_152733) do
+ActiveRecord::Schema.define(version: 2020_03_02_115026) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.string "author", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "point1", null: false
-    t.text "point2"
-    t.text "point3"
-    t.text "impression", null: false
-    t.bigint "user_id", null: false
-    t.bigint "memo_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["memo_id"], name: "index_memos_on_memo_id"
-    t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -45,6 +32,4 @@ ActiveRecord::Schema.define(version: 2020_02_24_152733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "memos", "memos"
-  add_foreign_key "memos", "users"
 end
