cb3142026920783c72d2f11152908f294c5c83ff
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-03 00:28:01 +0900

    Update 20200302152118_add_details_to_books.rb

--- db/migrate/20200302152118_add_details_to_books.rb
+++ db/migrate/20200302152118_add_details_to_books.rb
@@ -1,9 +1,9 @@
 class AddDetailsToBooks < ActiveRecord::Migration[5.2]
   def change
-    add_column :books, :point1, :text
+    add_column :books, :point1, :text, null: false
     add_column :books, :point2, :text
     add_column :books, :point3, :text
-    add_column :books, :impression, :text
-    add_column :books, :user_id, :reference
+    add_column :books, :impression, :text, null: false
+    add_column :books, :user_id, :references, null: false
   end
 end
