f01875018d063c6cea144cae8a355a3fd73790fc
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-03 00:22:15 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_115026) do
+ActiveRecord::Schema.define(version: 2020_02_24_141652) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
