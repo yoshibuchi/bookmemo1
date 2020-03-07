bc4aa11fedd86274aa54ffbb4a8a6a4bd4927721
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-07 19:12:20 +0900

    post create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,5 +4,16 @@ class BooksController < ApplicationController
     end
 
     def new
+        @book = Book.new
     end
+
+    def create
+      @book = Book.create(book_params)
+    end
+
+
+    private
+      def book_params
+        params.require(:book).permit(:title, :author, :point1, :point2, :point3, :impression)
+      end
 end
