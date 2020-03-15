f4ff78315fa4fb13880a08d5e303154ecc1fa6cb
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-15 23:41:12 +0900

    devise logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
  
   protected
   def configure_permitted_parameters
