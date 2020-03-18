bc1b65c04e207e2e0a99f919ee7f7a2488c0f49e
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-18 20:55:10 +0900

    protect_from_forgery destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery :except => ["destroy"]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
