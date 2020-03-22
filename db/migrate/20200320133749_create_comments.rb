906edc8fcde6be87d718c27cdebbc039448136fb
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-22 17:17:34 +0900

    Update 20200320133749_create_comments.rb

--- db/migrate/20200320133749_create_comments.rb
+++ db/migrate/20200320133749_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.string :content
+      t.text :text
       t.references :book, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
