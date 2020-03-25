86d8d1c5ae6c3a3d70889f1e8705999dac9d789d
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-25 22:47:36 +0900

    nickname fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by(id: params[:id])
         @books = current_user.books
+        @nickname = current_user.nickname
     end
 end
