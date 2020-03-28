35ee37b13f0f41b9febf57a81157dab8bb7ae153
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-28 19:32:35 +0900

    controller user find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @user = User.find_by(id: params[:id])
         @books = current_user.books
         @nickname = current_user.nickname
     end
