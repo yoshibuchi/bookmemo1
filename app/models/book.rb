08205402c33644b5e1eccfb312337fed63c792ca
yoshibuchi <yoshibuchi.n@gmail.com>
2020-03-29 18:13:24 +0900

    book.rb validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true, length: {maximum:20}
+  validates :author, presence: true, length: {maximum:10}
+  validates :point1, presence: true, length: {maximum:20}
+  validates :impression, presence: true, length: {maximum:300}
 end
